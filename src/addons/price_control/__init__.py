953692011f40654d775d723ecea6597daab38827
Cytex124 <andreas.wyrobek95@gmail.com>
2019-06-04 23:32:18 +0200

    [ADD] Custom App Verbose Name

---
+++ src/addons/price_control/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "addons.price_control.apps.PriceControlConfig"
--- src/addons/price_control/apps.py
+++ src/addons/price_control/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class PriceControlConfig(AppConfig):
     name = 'addons.price_control'
+    verbose_name = "Price Control"
